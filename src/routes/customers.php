0fb993e9aa27c9ebca7b369e2c4032369d1a2b09
Alan <alanjmurphy91@gmail.com>
2019-02-03 22:39:49 +0000

    update

---
+++ src/routes/customers.php
@@ -0,0 +1,40 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App;
+
+// Get All Customers
+$app->get('/api/customers', function(Request $request, Response $response) {
+    $sql = "SELECT * FROM customers";
+
+    try {
+        $db = new db();
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($customers);
+    } catch(PDOException $e) {
+        echo '{"error": {"text": '.$e->getMessage().'}}';
+    }
+});
+
+// Get Single Customer
+$app->get('/api/customer/{id}', function(Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "SELECT * FROM customers WHERE id = $id";
+
+    try {
+        $db = new db();
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customer = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($customer);
+    } catch(PDOException $e) {
+        echo '{"error": {"text": '.$e->getMessage().'}}';
+    }
+});
